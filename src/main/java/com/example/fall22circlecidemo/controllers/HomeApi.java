0b0c336d61841aba5845631e6de5e90a66be2428
conor <Obrien.Conor@student.greenriver.edu>
2022-11-29 14:10:27 -0800

    updated to have a comprehensive homeTest

--- src/main/java/com/example/fall22circlecidemo/controllers/HomeApi.java
+++ src/main/java/com/example/fall22circlecidemo/controllers/HomeApi.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class HomeApi
 {
-    @GetMapping
+    @GetMapping("home")
     public String home()
     {
         return "Hello, world....";
--- src/test/java/com/example/fall22circlecidemo/Fall22CircleCiDemoApplicationTests.java
+++ src/test/java/com/example/fall22circlecidemo/Fall22CircleCiDemoApplicationTests.java
@@ -1,15 +1,39 @@
 package com.example.fall22circlecidemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Fall22CircleCiDemoApplicationTests
 {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
-    void contextLoads()
+    public void contextLoads()
     {
     }
 
+    @Test
+    public void homeTest()
+    {
+        //create a request for http://localhost:<port>/api/v1/home
+        String url = "http://localhost:" + port + "/api/v1/home";
+        HttpEntity<String> request = new HttpEntity<>(new HttpHeaders());
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+
+        //test whether it worked or not
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(), "Hello, world....");
+    }
 }
