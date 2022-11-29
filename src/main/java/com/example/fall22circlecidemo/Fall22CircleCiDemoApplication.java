ff90f7fa2f8c242572cb8ae75209fe8836669ec6
conor <Obrien.Conor@student.greenriver.edu>
2022-11-29 13:38:27 -0800

    added starter files for project

---
+++ src/main/java/com/example/fall22circlecidemo/Fall22CircleCiDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.fall22circlecidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fall22CircleCiDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Fall22CircleCiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fall22circlecidemo/Fall22CircleCiDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.fall22circlecidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fall22CircleCiDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
